6728473ac5258cdc72728d5f4a056fca70490dd2
AFDean00 <afdean00@gmail.com>
2017-02-21 18:25:39 -0500

    feat: Add first draft of question models and initial fixture

--- fallprevention/app/models.py
+++ fallprevention/app/models.py
@@ -1,5 +1,35 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+# Model for logging key questions in database
+@python_2_unicode_compatible
+class KeyQuestion(models.Model):
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+# Model for logging additional questions in database
+@python_2_unicode_compatible
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+# Model for logging medication list in database
+class Medication(models.Model):
+    medication_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+
+@python_2_unicode_compatible
+class DummyModel(models.Model):
+    dummy_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.dummy_text
