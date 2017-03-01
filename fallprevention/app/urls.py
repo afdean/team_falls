60f76a13ce93a40351fa9fa672ebe0c9d78fce1a
urbanintell <luiikromah@gmail.com>
2017-02-28 23:18:44 -0500

    Merged Project

--- fallprevention/app/urls.py
+++ fallprevention/app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from app import views as app_views
+from . import views
 
 urlpatterns = [
-    url(r'^$', app_views.index, name="index"),
+    url(r'^app/$', app_views.index, name="index"),
     url(r'^questions/$', app_views.questions, name="questions"),
     url(r'^funcabilitytests/$', app_views.test_list, name="test_list"),
-      url(r'^login/$', views.user_login, name='login'),
+    url(r'^login/$', views.user_login, name='login'),
 ]
 
