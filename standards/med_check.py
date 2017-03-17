9ed160032feb4cae5c4387c557303f71b8b91d8a
AFDean00 <afdean00@gmail.com>
2017-03-17 16:35:34 -0400

    Fix errors in medication.json using med_check script

--- standards/med_check.py
+++ standards/med_check.py
@@ -1,2 +1,30 @@
-# Check for each pair in order if equivalent
-# Check that they are all length 10
+# from __future__ import print_function
+import json
+
+def run():
+    '''
+    Function to check if medication file was formatted correctly
+    '''
+
+    med_file = open("medication.json", "r")
+    med = json.load(med_file)
+    gpi_list = []
+
+    for i in range(0, len(med)):
+        for j in range(0, len(med[i].get("gpi_codes"))):
+            gpi_list.append(med[i].get("gpi_codes")[j])
+
+    for code in gpi_list:
+        if code < 1000000000 or code > 9999999999:
+            print("Incorrectly formatted code: " + code)
+
+    for i in range(0, len(gpi_list) - 1):
+        if gpi_list[i] == gpi_list[i + 1]:
+            print("Incorrectly formatted code: " + str(gpi_list[i]) + " " + str(gpi_list[i + 1]))
+
+    for i in range(0, len(gpi_list) - 2):
+        if gpi_list[i] == gpi_list[i + 2]:
+            print("Incorrectly formatted code: " + str(gpi_list[i]) + " " + str(gpi_list[i + 2]))
+
+if __name__ == '__main__':
+    run()
