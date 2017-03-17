0051ad58779e49d224e4e8be99b0fa8f9aec11cd
AFDean00 <afdean00@gmail.com>
2017-03-16 23:53:54 -0400

    Reconstruct json for medication using new method

---
+++ standards/write_med.py
@@ -0,0 +1,21 @@
+def run():
+    med_file = open("medication_data.txt", "r")
+    med = open("medication.json", "w")
+
+    med_list = med_file.readlines()
+    med.write("[\n")
+
+    for i in range(0, len(med_list)):
+        med.write("  {\n")
+        med.write("    \"name\": \"" + med_list[i][:-1] + "\",\n")
+        med.write("    \"date\": \"" + "2015-12" + "\",\n")
+        med.write("    \"gpi_codes\": []\n")
+        if i < len(med_list) - 1:
+            med.write("  },\n")
+        else:
+            med.write("  }\n")
+
+    med.write("]\n")
+
+if __name__ == '__main__':
+    run()
