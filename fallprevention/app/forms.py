90d753750ee98698c61b60f1aaf0b2a4b95d5414
AFDean00 <afdean00@gmail.com>
2017-03-27 23:24:34 -0400

    Rewrite generate_form in forms

--- fallprevention/app/forms.py
+++ fallprevention/app/forms.py
@@ -9,25 +9,27 @@ from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 from .models import Question
 from app.data_client import DataClient
 
-def generateForm(field, fieldName, form, isHidden):
-    if (field.type == "boolean"):
-        form.fields[fieldName] = forms.BooleanField(
-            label = field.content,
-            required = False,
+def generate_form(field, field_widget, field_choices, isHidden=None):
+    if field.type == "boolean":
+        return forms.BooleanField(
+            label=field.content,
+            widget=field_widget,
+            choices=field_choices,
+            required=False,
         )
 
     elif (field.type == "integer"):
-        form.fields[fieldName] = forms.IntegerField(
+        self.fields[field_name] = forms.IntegerField(
             label = field.content,
             required = False,
         )
     elif (field.type == "char"):
-        form.fields[fieldName] = forms.CharField(
+        self.fields[field_name] = forms.CharField(
             label = field.content,
             required = False,
         )
     if (isHidden):
-        form.fields[fieldName].widget = forms.HiddenInput()
+        self.fields[field_name].widget = forms.HiddenInput()
 
 class LoginForm(forms.Form):
     identity = forms.TypedChoiceField(
@@ -42,7 +44,6 @@ class LoginForm(forms.Form):
         self.helper.form_id = 'id-loginForms'
         self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-
         self.helper.add_input(Submit('submit', 'Next'))
 
 class LoginCPForm(forms.Form):
@@ -57,14 +58,12 @@ class LoginCPForm(forms.Form):
         max_length = 80,
         required = True,
     )
-
     def __init__(self, *args, **kwargs):
         super(LoginCPForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'id-loginCPForms'
         self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-
         self.helper.add_input(Submit('submit', 'Next'))
 
 class SearchPatientForm(forms.Form):
@@ -90,12 +89,14 @@ class QuestionForm(forms.Form):
         data_client = DataClient()
         CHOICES = (('1', 'Yes',), ('2', 'No',))
         for i, question in enumerate(data_client.questions.questions):
-            fieldName = "question" + str(i)
-            self.fields[fieldName] = forms.ChoiceField(
-                label = question.content,
-                widget=forms.RadioSelect, choices = CHOICES,
-                required = False,
-            )
+            field_name = "question" + str(i)
+            self.fields[field_name] = generate_form(question, forms.RadioSelect, CHOICES)
+            # self.fields[field_name] = forms.ChoiceField(
+            #     label = question.content,
+            #     widget= forms.RadioSelect,
+            #     choices = CHOICES,
+            #     required = False,
+            # )
         self.helper = FormHelper()
         self.helper.form_id = 'id-questionsForm'
         self.helper.form_method = 'post'
@@ -119,18 +120,41 @@ class TugForm(forms.Form):
         required = False,
         widget = forms.CheckboxSelectMultiple,
     )
-class ThirtySecStandForm(forms.Form):
-     cs_test_details = forms.CharField(
-        label = 'Score:',
-        required = False,
-    )
 
-def __init__(self, *args, **kwargs):
+class TugForm2(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(TugForm2, self).__init__(*args, **kwargs)
+        data_client = DataClient()
+        CHOICES = (('1', 'Yes',), ('2', 'No',))
+        for test in data_client.func_test.func_test:
+            if test.name == "Timed Up and Go Test":
+                tug_test = test
+                break
+        for i, form in enumerate(tug_test.forms):
+            field_name = "form" + str(i)
+            self.fields[fieldName] = generate_form(form, field_name)
+            self.fields[fieldName] = forms.ChoiceField(
+                label = question.content,
+                widget=forms.RadioSelect, choices = CHOICES,
+                required = False,
+            )
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-tugform2'
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+class ThirtySecStandForm(forms.Form):
+    def __init__(self, *args, **kwargs):
         super(ThirtySecStandForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'id-balance'
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Submit'))
+
+    cs_test_details = forms.CharField(
+        label = 'Score:',
+        required = False,
+    )
 
 class BalanceTestForm(forms.Form):
     balance_test_detail1 = forms.CharField(
@@ -167,20 +191,22 @@ class NoteForm(forms.Form):
 
 class MedicationsForm(forms.Form):
     #hard code medications for now, will generate it dynamically next step
-     asprin = forms.CharField(
-        label = 'Asprin',
-        required = False,
-    )
-isosorbide = forms.CharField(
-        label = 'Isosorbide',
-        required = False,
-    )
-def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(MedicationsForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'id-problemsForms'
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Submit'))
+
+    asprin = forms.CharField(
+        label = 'Asprin',
+        required = False,
+    )
+    isosorbide = forms.CharField(
+        label = 'Isosorbide',
+        required = False,
+    )
+
 
 class ProblemsForm(forms.Form):
     problems = forms.CharField(
