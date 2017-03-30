233275491c49975f65e13b980ae74eaec3c537ad
Ari Kapusta <akapusta@gatech.edu>
2017-03-30 16:51:34 -0400

    fixed thing for ubuntu? unclear why it only did not work for me, but now works. creates object from the online document

--- fallprevention/app/data_client.py
+++ fallprevention/app/data_client.py
@@ -4,7 +4,7 @@ import urllib.request as ur
 from .constants import *
 
 def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
-def json2obj(data): return json.load(data, object_hook=_json_object_hook)
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
 
 class Singleton(type):
     def __init__(self, *args, **kwargs):
@@ -20,15 +20,17 @@ class Singleton(type):
 
 class DataClient(metaclass=Singleton):
     def __init__(self):
+        print('I GOT HERE WOOO')
         with ur.urlopen(QUESTIONS_URL) as url_questions:
-            self.questions = json2obj(url_questions)
+            self.questions = json2obj(url_questions.read().decode('utf8'))
         with ur.urlopen(FAT_URL) as url_fat:
-            self.func_test = json2obj(url_fat)
+            self.func_test = json2obj(url_fat.read().decode('utf8'))
         with ur.urlopen(MEDICATION_URL) as url_medication:
-            self.medication = json2obj(url_medication)
+            self.medication = json2obj(url_medication.read().decode('utf8'))
         with ur.urlopen(PHYEXAM_URL) as url_phyexam:
-            self.physical_exam = json2obj(url_phyexam)
+            self.physical_exam = json2obj(url_phyexam.read().decode('utf8'))
         with ur.urlopen(INTERVENTION_URL) as url_intervention:
-            self.intervention_list = json2obj(url_intervention)
+            self.intervention_list = json2obj(url_intervention.read().decode('utf8'))
         with ur.urlopen(RISK_URL) as url_risk:
-            self.risk_list = json2obj(url_risk)
+            self.risk_list = json2obj(url_risk.read().decode('utf8'))
+        print(self.questions)
