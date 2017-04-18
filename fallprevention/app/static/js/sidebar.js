82543275283afa6a6898d9321d2398109e2217b7
urbanintell <luiikromah@gmail.com>
2017-04-17 21:41:54 -0400

    strikethrough sidebar items when completed

--- fallprevention/app/static/js/sidebar.js
+++ fallprevention/app/static/js/sidebar.js
@@ -3,10 +3,17 @@
 var main = function () {
   "use strict";
   $(".nav a").on("click", function () {
-    
+
     $("#userMenu").find(".active").removeClass("active");
     $(this).parent().addClass("active");
     console.log($(this))
   });
 }
 $(document).ready(main);
+
+function completedTask(tasks) {
+  $("#pic1").click(function () {
+    $("#items p").wrap("<del>");
+    $("#pic1").fadeOut("slow");
+  });
+}
